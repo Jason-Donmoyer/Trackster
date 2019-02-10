45eb3ff6aa69ffbe7e230817351ab79140a5bf51
Jason-Donmoyer <j.donmoyer523@gmail.com>
2019-02-10 08:04:22 -0500

    working version

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -12,7 +12,28 @@ $(document).ready(function() {
   Append each "row" to the container in the body to display all tracks. 
 */
 Trackster.renderTracks = function(tracks) {
+  let $trackList = $('#track-list');
 
+  $trackList.empty();
+
+  for (let trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+    let track = tracks[trackIndex];
+    let mediumAlbumArt = track.image[1]['#text'];
+    let htmlTrackRow =
+    '<div class="row track">' + 
+      '<div class="col-xs-1 col-xs-offset-1 play-button">' + 
+        '<a href="' + track.url + '" target="_blank">' +
+          '<i class="fa fa-play-circle-o fa-2x"></i>' +
+        '</a>' +
+      '</div>' +
+      '<div class="col-xs-4">' + track.name + '</div>' +
+      '<div class="col-xs-2">' + track.artist + '</div>' +
+      '<div class="col-xs-2"><img src="' + mediumAlbumArt + '"></div>' +
+      '<div class="col-xs-2">' + track.listeners + '</div>' +
+    '</div>';
+
+    $trackList.append(htmlTrackRow);
+  }
 };
 
 /*
@@ -23,7 +44,7 @@ Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json',
     success: function(response) {
-      console.log(response);
+      Trackster.renderTracks(response.results.trackmatches.track);
     }
   });
 };
